6f3ad1e3d20bd276801301f874d17b9eca1d5da9
GinoLin980 <ppple1872@gmail.com>
2024-07-19 01:18:18 +0800

    add comments and type hints

--- sort_visualizer.py
+++ sort_visualizer.py
@@ -6,13 +6,8 @@ from OpenGL.GL import *
 from pygame.locals import *
 from sorting_algorithms import SortingAlgorithms
 
-def reso(resolution, direction): # return 16:9
-    if direction == "p":
-        return resolution, resolution*16/9
-    if direction == "l":
-        return resolution*16/9, resolution
 
-available_algo = ["bubble_sort", "quick_sort"]
+available_algo: list[str] = ["bubble_sort", "quick_sort"]
 
 if len(sys.argv) != 2:
     print(f"\nUsage: python3 sort_visualizer_OpenGL.py {' or '.join(available_algo)}\n")
@@ -22,21 +17,34 @@ sort_algorithm = sys.argv[1].lower()
 
 assert sort_algorithm in available_algo, f"\nThis {sort_algorithm} is not available now\nPlease choose from {','.join(available_algo)}\n"
 
+def reso(resolution: int, direction: str) -> int:
+    """Return 16:9 resolution.
+    
+    params: int, str ["p", "l"]
+
+    Example:
+    reso(1080, l) -> 1920, 1080
+    """
+    if direction.lower() == "p":
+        return resolution, resolution*16/9
+    if direction.lower() == "l":
+        return resolution*16/9, resolution
+    
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((reso(1080, "l")), DOUBLEBUF | OPENGL)
-pygame.display.set_caption('Pygame OpenGL Rectangle')
+pygame.display.set_caption('Sorting Algorithm Visualizer With Pygame and OpenGL')
 
 # Constants
-BAR_COLOR = (1, 0.603, 0.541)
-COMPARE_COLOR = (1, 0, 1)
+BAR_COLOR: tuple[float] = (1, 0.603, 0.541) # Peach Pink
+COMPARE_COLOR: tuple[float] = (1, 0, 1) # Purple
 WIDTH = screen.get_width()
 HEIGHT = screen.get_height()
-ARR = np.arange(1, 512 + 1)
+ARR = np.arange(1, 1024 + 1)
 np.random.shuffle(ARR)
 N = len(ARR)
 BAR_WIDTH = WIDTH / N
-HEIGHT_UNIT = HEIGHT / max(ARR)
+HEIGHT_UNIT: float = HEIGHT / max(ARR)
 MARGIN = 100 / N
 
 
@@ -45,15 +53,14 @@ glClearColor(0.0, 0.0, 0.0, 1.0)
 glClear(GL_COLOR_BUFFER_BIT)
 glMatrixMode(GL_PROJECTION)
 glLoadIdentity()
-glOrtho(0, WIDTH, 0, HEIGHT, -1, 1)
+glOrtho(0, WIDTH, 0, HEIGHT, -1, 1) # set the start(origin) point from the bottom left of the window
 glMatrixMode(GL_MODELVIEW)
 glLoadIdentity()
 
 
-def draw_bar(index: int, value: float | int, color: tuple[float]) -> None: # 左下 右下 右上 左上
-
-    r, g, b = (i for i in color)
-    glColor3f(r, g, b)
+def draw_bar(index: int, value: float | int, color: tuple[float]) -> None:
+    """Use OpenGL to draw rectangles."""
+    glColor3f(*color)
     glBegin(GL_QUADS)
     glVertex2f(BAR_WIDTH * index, 0)
     glVertex2f(BAR_WIDTH * (index+1) - MARGIN, 0)
@@ -61,17 +68,15 @@ def draw_bar(index: int, value: float | int, color: tuple[float]) -> None: # 左
     glVertex2f(BAR_WIDTH * index, HEIGHT_UNIT * value)
     glEnd()
 
-# GO FIX THE BUBBLE SORT DISPLAY ISSUE
-
-
-def init_bar(arr):
+def init_bar(arr) -> None:
+    """Draw out all the shuffled array."""
     for i, value in enumerate(arr):
         draw_bar(i, value, BAR_COLOR)
 
 # Main loop
 init_bar(ARR)
 pygame.display.flip()
-sorting_algo = SortingAlgorithms(ARR, screen, WIDTH, HEIGHT, MARGIN, BAR_COLOR, COMPARE_COLOR, (0, 0, 0), 0.005, True)
+sorting_algo = SortingAlgorithms(ARR, screen, WIDTH, HEIGHT, MARGIN, BAR_COLOR, COMPARE_COLOR, (0, 0, 0), 0.005)
 running = True
 
 if sort_algorithm == "quick_sort":
@@ -81,7 +86,7 @@ elif sort_algorithm == "bubble_sort":
     action = sorting_algo.bubble_sort
     params = (None, None)
 else:
-    print("Invalid sorting algorithm. Choose 'quicksort' or 'bubblesort'.")
+    print("Invalid sorting algorithm. Choose 'quick_sort' or 'bubble_sort'.")
     running = False
 
 while running:
@@ -92,7 +97,7 @@ while running:
     action(*params)
 
     if np.array_equal(ARR, np.sort(ARR)):
-        time.sleep(3)
+        time.sleep(1)
         break
 
 pygame.quit()
--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -4,8 +4,8 @@ import pygame
 from OpenGL.GL import *
 
 class SortingAlgorithms:
-    def __init__(self, lst, screen, width, height, margin, bar_color, compare_color, bg_color, wait_time, USE_GL=False):
-        self.lst = lst
+    def __init__(self, arr: list[int], screen: pygame.Surface, width: int, height: int, margin: float, bar_color: tuple[float], compare_color: tuple[float], bg_color: tuple[float], wait_time: float):
+        self.arr = arr
         self.screen = screen
         self.width = width
         self.height = height
@@ -14,20 +14,13 @@ class SortingAlgorithms:
         self.compare_color = compare_color
         self.bg_color = bg_color
         self.wait_time = wait_time
-        self.n = len(self.lst)
+        self.n = len(self.arr)
         self.bar_width = self.width / self.n
-        self.height_unit = self.height / max(self.lst)
-        self.USE_GL = USE_GL
-        self.draw = self.draw_bar_GL if self.USE_GL else self.draw_bar
+        self.height_unit = self.height / max(self.arr)
 
-    def draw_bar(self, index, value, color):
-        rect = pygame.Rect(index * self.bar_width, self.height - value * self.height_unit, self.bar_width - self.margin, value * self.height_unit)
-        pygame.draw.rect(self.screen, color, rect)
-        pygame.display.update(rect)
-
-    def draw_bar_GL(self, index, value, color):
-        r, g, b = color
-        glColor3f(r, g, b)
+    def draw_bar(self, index: int, value: int, color: tuple[float]) -> None:
+        """Use OpenGL to draw rectangles."""
+        glColor3f(*color)
         glBegin(GL_QUADS)
         glVertex2f(self.bar_width * index, 0)
         glVertex2f(self.bar_width * (index+1) - self.margin, 0)
@@ -35,61 +28,67 @@ class SortingAlgorithms:
         glVertex2f(self.bar_width * index, self.height_unit * value)
         glEnd()
 
-    def swap_bars(self, i, j):
-        self.lst[i], self.lst[j] = self.lst[j], self.lst[i]
-        self.draw(i, self.lst[i], self.bar_color)
-        self.draw(j, self.lst[j], self.bar_color)
+    def swap_bars(self, i: int, j: int) -> None:
+        """Draw the value of each other bar."""
+        self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+        self.draw_bar(i, self.arr[i], self.bar_color)
+        self.draw_bar(j, self.arr[j], self.bar_color)
 
-    def update_display(self):
-        if self.USE_GL:
-            glClear(GL_COLOR_BUFFER_BIT)
-            for k, value in enumerate(self.lst):
-                self.draw(k, value, self.bar_color)
-            pygame.display.flip()
-            # time.sleep(self.wait_time)
-        else:
-            pygame.display.update()
+    def update_display(self) -> None:
+        glClear(GL_COLOR_BUFFER_BIT)
+        for k, value in enumerate(self.arr):
+            self.draw_bar(k, value, self.bar_color)
+        pygame.display.flip()
+        # time.sleep(self.wait_time)
+
+    def check_quit(self) -> None:
+        """Check quit state in the for loop"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
     def bubble_sort(self, *args):
-        n = len(self.lst)
+        n = len(self.arr)
         for i in range(n):
             swapped = False
             for j in range(0, n - i - 1):
-                self.draw(j, self.lst[j], self.compare_color)
-                self.draw(j + 1, self.lst[j + 1], self.compare_color)
+                self.draw_bar(j, self.arr[j], self.compare_color)
+                self.draw_bar(j + 1, self.arr[j + 1], self.compare_color)
+                self.check_quit()
                 pygame.display.flip()
                 time.sleep(self.wait_time)
 
-                if self.lst[j] > self.lst[j + 1]:
+                if self.arr[j] > self.arr[j + 1]:
                     self.swap_bars(j, j + 1)
                     swapped = True
 
-                self.draw(j, self.lst[j], self.bar_color)
-                self.draw(j + 1, self.lst[j + 1], self.bar_color)
+                self.draw_bar(j, self.arr[j], self.bar_color)
+                self.draw_bar(j + 1, self.arr[j + 1], self.bar_color)
                 self.update_display()
 
             if not swapped:
                 break
 
-    def GL_partition(self, low, high):
-        i = (low-1)
-        pivot = self.lst[high]
+    def partition(self, low: int, high: int) -> int:
+        i = low - 1
+        pivot = self.arr[high]
 
         for j in range(low, high):
-            if self.lst[j] <= pivot:
+            self.check_quit()
+            if self.arr[j] <= pivot:
                 i = i+1
-                self.lst[i], self.lst[j] = self.lst[j], self.lst[i]
+                self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
                 glClear(GL_COLOR_BUFFER_BIT)
-                for k, value in enumerate(self.lst):
+                for k, value in enumerate(self.arr):
                     color = self.compare_color if k == i or k == j else self.bar_color
-                    self.draw(k, value, color)
+                    self.draw_bar(k, value, color)
                 pygame.display.flip()
                 pygame.time.wait(0)
-        self.lst[i+1], self.lst[high] = self.lst[high], self.lst[i+1]
-        return (i+1)
+        self.arr[i+1], self.arr[high] = self.arr[high], self.arr[i+1]
+        return i + 1
 
-    def quick_sort(self, low, high):
+    def quick_sort(self, low: int, high: int):
         if low < high:
-            pi = self.partition(low, high) if not self.USE_GL else self.GL_partition(low, high)
+            pi = self.partition(low, high)
             self.quick_sort(low, pi - 1)
             self.quick_sort(pi + 1, high)
