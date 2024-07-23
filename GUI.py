a694fd576f9768c657d9538fb59dce50ebfacbb6
GinoLin980 <ppple1872@gmail.com>
2024-07-23 23:42:39 +0800

    add recorder option but currently can't run on Ubuntu

--- GUI.py
+++ GUI.py
@@ -1,4 +1,7 @@
-import sys; sys.dont_write_bytecode = True
+import sys;
+import webbrowser
+
+sys.dont_write_bytecode = True
 import customtkinter as ctk
 
 
@@ -6,7 +9,7 @@ class Sort_Visualizer_GUI(ctk.CTk):
     def __init__(self, settings: dict[str, str|int]):
         super().__init__()
         self.title("Sorting Visualizer")
-        self.geometry("360x185")
+        self.geometry("360x270")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.running = True
         self.grid_rowconfigure(0, weight=0)
@@ -16,6 +19,11 @@ class Sort_Visualizer_GUI(ctk.CTk):
         self.orient = ctk.StringVar(value="Landscape")
         self.elements = ctk.IntVar(value=512)
         self.algo = ctk.StringVar(value="Quick Sort")
+        self.toggle_record_var = ctk.StringVar(value="on")
+        self.toggle_record = False
+
+        def record_assign():
+            self.toggle_record = True if self.toggle_record_var.get() == "1" else False
 
         label1 = ctk.CTkLabel(self, text="Please Choose the Options and Hit GO", font=ctk.CTkFont(size=18))
         reso_label = ctk.CTkLabel(self, text="Resolution:", font=ctk.CTkFont(size=14))
@@ -26,7 +34,9 @@ class Sort_Visualizer_GUI(ctk.CTk):
         elements_option = ctk.CTkOptionMenu(self, values=["10", "50", "100", "256", "512", "1024", "2048"], variable=self.elements, command=lambda x: self.elements.set(x))
         algo_label = ctk.CTkLabel(self, text="Algorithm:", font=ctk.CTkFont(size=14))
         algo_option = ctk.CTkOptionMenu(self,values=["Bubble Sort", "Quick Sort"], variable=self.algo, command=lambda x: self.algo.set(x))
-        go_button = ctk.CTkButton(self, text="GO", fg_color="lime green", text_color="black", font=ctk.CTkFont(size=13, weight="bold"), command=self.GO_callback)
+        record_check = ctk.CTkSwitch(self, text="Record this sort once(reset after sort)", variable=self.toggle_record_var, command=record_assign, onvalue=True, offvalue=False)
+        go_button = ctk.CTkButton(self, text="GO", fg_color="lime green", text_color="black", hover_color="dark green", font=ctk.CTkFont(size=13, weight="bold"), command=self.GO_callback)
+        forza_button = ctk.CTkButton(self, text="Check out my Forza Real Gearbox", text_color='black', fg_color="PeachPuff2", hover_color="PeachPuff4", font=ctk.CTkFont(size=13, weight="bold"), command=lambda :webbrowser.open("https://www.github.com/GinoLin980/Forza-Horizon-realistic-gearbox"))
 
         label1.grid(row=0, column=0, sticky="ew", columnspan=2)
         reso_label.grid(row=1, column=0, padx=25, sticky="w")
@@ -37,10 +47,13 @@ class Sort_Visualizer_GUI(ctk.CTk):
         algo_label.grid(row=3, column=1, padx=25, sticky="w")
         elements_option.grid(row=4, column=0, padx=20, sticky="w")
         algo_option.grid(row=4, column=1, padx=20, sticky="w")
-        go_button.grid(row=5, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        record_check.grid(row=5, column=0, padx=25, pady=10, sticky="ew", columnspan=2)
+        go_button.grid(row=6, column=0, padx=10, pady=5, sticky="ew", columnspan=2)
+        forza_button.grid(row=7, column=0, padx=10, pady=5, sticky="ew", columnspan=2)
+
 
     def GO_callback(self):
-        self.settings = {"reso": int(self.reso.get()[:-1]), "orient": self.orient.get(), "elements": self.elements.get(), "algo": self.algo.get(), "Start_Visual": True}
+        self.settings = {"reso": int(self.reso.get()[:-1]), "orient": self.orient.get(), "elements": self.elements.get(), "algo": self.algo.get(), "Start_Visual": True, "record_this_sort": self.toggle_record}
 
     def get_settings(self):
         return self.settings
--- sort_visualizer.py
+++ sort_visualizer.py
@@ -6,103 +6,120 @@ from OpenGL.GL import *
 from pygame.locals import *
 from sorting_algorithms import SortingAlgorithms
 from GUI import Sort_Visualizer_GUI
+from pygame_screen_record import ScreenRecorder
 
 BAR_COLOR: tuple[float] = (1, 0.603, 0.541)  # Peach Pink
 COMPARE_COLOR: tuple[float] = (1, 0, 1)  # Purple
 settings = {"Start_Visual": False}
 APP = Sort_Visualizer_GUI(settings)
+
 while True:
     APP.run()
     settings = APP.get_settings()
     if settings["Start_Visual"]:
-        def reso(resolution: int, direction: str) -> tuple[int, int]:
-            """Return 16:9 resolution.
-
-            params: int, str ["p", "l"]
-
-            Example:
-            reso(1080, l) -> 1920, 1080
-            """
-            if direction.lower() == "portrait":
-                return resolution, resolution * 16 / 9
-            if direction.lower() == "landscape":
-                return resolution * 16 / 9, resolution
-
-
-        # Initialize Pygame
-        pygame.init()
-        screen = pygame.display.set_mode((reso(settings["reso"], settings["orient"])), DOUBLEBUF | OPENGL)
-        pygame.display.set_caption('Sorting Algorithm Visualizer With Pygame and OpenGL')
-
-        # Constants
-        WIDTH = screen.get_width()
-        HEIGHT = screen.get_height()
-        ARR = np.arange(1, settings["elements"] + 1)
-        np.random.shuffle(ARR)
-        N = len(ARR)
-        BAR_WIDTH = WIDTH / N
-        HEIGHT_UNIT: float = HEIGHT / max(ARR)
-        MARGIN = 100 / N
-
-        # OpenGL settings
-        glClearColor(0.0, 0.0, 0.0, 1.0)
-        glClear(GL_COLOR_BUFFER_BIT)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0, WIDTH, 0, HEIGHT, -1, 1)  # set the start(origin) point from the bottom left of the window
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-
-
-        def draw_bar(index: int, value: float | int, color: tuple[float]) -> None:
-            """Use OpenGL to draw rectangles."""
-            glColor3f(*color)
-            glBegin(GL_QUADS)
-            glVertex2f(BAR_WIDTH * index, 0)
-            glVertex2f(BAR_WIDTH * (index + 1) - MARGIN, 0)
-            glVertex2f(BAR_WIDTH * (index + 1) - MARGIN, HEIGHT_UNIT * value)
-            glVertex2f(BAR_WIDTH * index, HEIGHT_UNIT * value)
-            glEnd()
-
-
-        def init_bar(arr) -> None:
-            """Draw out all the shuffled array."""
-            for i, value in enumerate(arr):
-                draw_bar(i, value, BAR_COLOR)
-
-
-        # Main loop
-        init_bar(ARR)
-        pygame.display.flip()
-        sorting_algo = SortingAlgorithms(ARR, screen, WIDTH, HEIGHT, MARGIN, BAR_COLOR, COMPARE_COLOR, (0, 0, 0), 0.005)
-        running = True
-
-        if settings["algo"].lower().replace(' ', '_') == "quick_sort":
-            action = sorting_algo.quick_sort
-            params = (0, N - 1)
-        elif settings["algo"].lower().replace(' ', '_') == "bubble_sort":
-            action = sorting_algo.bubble_sort
-            params = (None, None)
-        else:
-            print("Invalid sorting algorithm. Choose 'quick_sort' or 'bubble_sort'.")
-            running = False
-
-        while running:
-            try:
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        running = False
-            except Exception as e:
-                pygame.quit()
-                break
-            action(*params)
-
-            if np.array_equal(ARR, np.sort(ARR)):
-                time.sleep(1)
-                break
-
         try:
-            pygame.quit()
-        except Exception as e:
-            pass
+            def reso(resolution: int, direction: str) -> tuple[int, int]:
+                """Return 16:9 resolution.
+
+                params: int, str ["p", "l"]
+
+                Example:
+                reso(1080, l) -> 1920, 1080
+                """
+                if direction.lower() == "portrait":
+                    return resolution, resolution * 16 / 9
+                if direction.lower() == "landscape":
+                    return resolution * 16 / 9, resolution
+
+
+            print(settings)
+            # Initialize Pygame
+            pygame.init()
+            screen = pygame.display.set_mode((reso(settings["reso"], settings["orient"])), DOUBLEBUF | OPENGL)
+            pygame.display.set_caption('Sorting Algorithm Visualizer With Pygame and OpenGL')
+            if settings["record_this_sort"] == True:
+                recorder = ScreenRecorder(fps=30, surf=screen)
+                recorder.start_rec()
+
+            # Constants
+            WIDTH = screen.get_width()
+            HEIGHT = screen.get_height()
+            ARR = np.arange(1, settings["elements"] + 1)
+            np.random.shuffle(ARR)
+            N = len(ARR)
+            BAR_WIDTH = WIDTH / N
+            HEIGHT_UNIT: float = HEIGHT / max(ARR)
+            MARGIN = 100 / N
+
+            # OpenGL settings
+            glClearColor(0.0, 0.0, 0.0, 1.0)
+            glClear(GL_COLOR_BUFFER_BIT)
+            glMatrixMode(GL_PROJECTION)
+            glLoadIdentity()
+            glOrtho(0, WIDTH, 0, HEIGHT, -1, 1)  # set the start(origin) point from the bottom left of the window
+            glMatrixMode(GL_MODELVIEW)
+            glLoadIdentity()
+
+
+            def draw_bar(index: int, value: float | int, color: tuple[float]) -> None:
+                """Use OpenGL to draw rectangles."""
+                glColor3f(*color)
+                glBegin(GL_QUADS)
+                glVertex2f(BAR_WIDTH * index, 0)
+                glVertex2f(BAR_WIDTH * (index + 1) - MARGIN, 0)
+                glVertex2f(BAR_WIDTH * (index + 1) - MARGIN, HEIGHT_UNIT * value)
+                glVertex2f(BAR_WIDTH * index, HEIGHT_UNIT * value)
+                glEnd()
+
+
+            def init_bar(arr) -> None:
+                """Draw out all the shuffled array."""
+                for i, value in enumerate(arr):
+                    draw_bar(i, value, BAR_COLOR)
+
+
+            # Main loop
+            init_bar(ARR)
+            pygame.display.flip()
+            sorting_algo = SortingAlgorithms(ARR, screen, WIDTH, HEIGHT, MARGIN, BAR_COLOR, COMPARE_COLOR, (0, 0, 0), 0.005)
+            running = True
+
+            if settings["algo"].lower().replace(' ', '_') == "quick_sort":
+                action = sorting_algo.quick_sort
+                algo_name = "Quick Sort"
+                params = (0, N - 1)
+            elif settings["algo"].lower().replace(' ', '_') == "bubble_sort":
+                action = sorting_algo.bubble_sort
+                algo_name = "Bubble Sort"
+                params = (None, None)
+            else:
+                print("Invalid sorting algorithm. Choose 'quick_sort' or 'bubble_sort'.")
+                running = False
+
+            while running:
+                try:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            running = False
+                except Exception as e:
+                    pygame.quit()
+                    break
+                action(*params)
+
+                if np.array_equal(ARR, np.sort(ARR)):
+                    time.sleep(1)
+                    break
+
+            try:
+                if settings["record_this_sort"] == True:
+                        recorder.stop_rec()
+                        recorder.save_recording(f"{algo_name}.avi")
+                        print("we've reached end of rec")
+                pygame.quit()
+            except Exception as e:
+                pass
+        except:
+            print("record error"); sys.exit()
+
         settings["Start_Visual"] = False
+        settings["record_this_sort"] = False
